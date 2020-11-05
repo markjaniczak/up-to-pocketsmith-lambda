36a99d8467554b147620e1a25d6b4117678dbc9d
markjaniczak <mark@janiczak.me>
2020-11-05 19:01:01 +1000

    fixed whitespace

--- src/index.ts
+++ src/index.ts
@@ -3,94 +3,94 @@ import Axios from "axios";
 import { createHmac } from "crypto";
 
 interface UpObject<Attributes extends any> {
-	type: string;
-	id: string;
-	attributes: Attributes;
-	relationships: {
-		[key: string]: {
-			data: {
-				type: string;
-				id: string;
-			};
-			links: {
-				related: string;
-			};
-		};
-	};
+  type: string;
+  id: string;
+  attributes: Attributes;
+  relationships: {
+    [key: string]: {
+      data: {
+        type: string;
+        id: string;
+      };
+      links: {
+        related: string;
+      };
+    };
+  };
 }
 
 interface UpWebhookEvent {
-	eventType: "TRANSACTION_CREATED" | "TRANSACTION_SETTLED" | "TRANSACTION_DELETED" | "PING";
-	/**
-	 * ISO8601 datetime String
-	 */
-	createdAt: string;
+  eventType: "TRANSACTION_CREATED" | "TRANSACTION_SETTLED" | "TRANSACTION_DELETED" | "PING";
+  /**
+   * ISO8601 datetime String
+   */
+  createdAt: string;
 }
 
 interface UpTransaction {
-	status: "HELD" | "SETTLED";
-	rawText: Nullable<string>;
-	description: string;
-	message: Nullable<string>;
-	amount: {
-		/**
-		 * ISO4217 Currency Code
-		 */
-		currencyCode: string;
-		/**
-		 * String representation of two decimal precision float
-		 */
-		value: string;
-		valueInBaseUnits: number;
-	};
-	/**
-	 * ISO8601 datetime String
-	 */
-	settledAt: Nullable<string>;
-	/**
-	 * ISO8601 datetime String
-	 */
-	createdAt: string;
+  status: "HELD" | "SETTLED";
+  rawText: Nullable<string>;
+  description: string;
+  message: Nullable<string>;
+  amount: {
+    /**
+     * ISO4217 Currency Code
+     */
+    currencyCode: string;
+    /**
+     * String representation of two decimal precision float
+     */
+    value: string;
+    valueInBaseUnits: number;
+  };
+  /**
+   * ISO8601 datetime String
+   */
+  settledAt: Nullable<string>;
+  /**
+   * ISO8601 datetime String
+   */
+  createdAt: string;
 }
 
 const { UP_SECRET_KEY, UP_BEARER_TOKEN, POCKETSMITH_API_KEY, ACCOUNT_MAPPINGS } = process.env;
 
 const addTransaction = async (transaction: UpObject<UpTransaction>) => {
-	const accountMappings = JSON.parse(ACCOUNT_MAPPINGS);
+  const accountMappings = JSON.parse(ACCOUNT_MAPPINGS);
 
-	const pocketsmithAccount = accountMappings[transaction.relationships.account.data.id];
+  const pocketsmithAccount = accountMappings[transaction.relationships.account.data.id];
 
-	const { attributes } = transaction;
+  const { attributes } = transaction;
 
-	await Axios.post(
-		`https://api.pocketsmith.com/v2/transaction_accounts/${pocketsmithAccount}/transactions`,
-		{
-			payee: attributes.description || attributes.rawText,
-			amount: attributes.amount.valueInBaseUnits / 100,
-			date: attributes.createdAt,
-			memo: attributes.message,
-			note: transaction.id,
-		},
-		{
-			headers: {
-				"X-Developer-Key": POCKETSMITH_API_KEY,
-			},
-		}
-	);
+  await Axios.post(
+    `https://api.pocketsmith.com/v2/transaction_accounts/${pocketsmithAccount}/transactions`,
+    {
+      payee: attributes.description || attributes.rawText,
+      amount: attributes.amount.valueInBaseUnits / 100,
+      date: attributes.createdAt,
+      memo: attributes.message,
+      note: transaction.id,
+    },
+    {
+      headers: {
+        "X-Developer-Key": POCKETSMITH_API_KEY,
+      },
+    }
+  );
 };
 
 const removeTransaction = async (transaction: UpObject<UpTransaction>) => {
-	const accountMappings = JSON.parse(ACCOUNT_MAPPINGS);
+  const accountMappings = JSON.parse(ACCOUNT_MAPPINGS);
 
-	const pocketsmithAccount = accountMappings[transaction.relationships.account.data.id];
+  const pocketsmithAccount = accountMappings[transaction.relationships.account.data.id];
 
-	const transactions = await Axios.get(`https://api.pocketsmith.com/v2/accounts/${pocketsmithAccount}/transactions`, {
-		params: {
-			search: transaction.id,
-		},
-		headers: {
-			"X-Developer-Key": POCKETSMITH_API_KEY,
-		},
+  const transactions = await Axios.get(`https://api.pocketsmith.com/v2/accounts/${pocketsmithAccount}/transactions`, {
+    params: {
+      search: transaction.id,
+    },
+    headers: {
+      "X-Developer-Key": POCKETSMITH_API_KEY,
+    },
   });
 
   if (transactions.data.length) {
@@ -103,59 +103,59 @@ const removeTransaction = async (transaction: UpObject<UpTransaction>) => {
 };
 
 export const handle = async (event: APIGatewayEvent): Promise<any> => {
-	if (!event.body) {
-		return {
-			statusCode: 400,
-		};
-	}
+  if (!event.body) {
+    return {
+      statusCode: 400,
+    };
+  }
 
-	const isAuthentic =
-		createHmac("sha256", UP_SECRET_KEY).update(event.body).digest("hex") ===
-		event.headers["X-Up-Authenticity-Signature"];
+  const isAuthentic =
+    createHmac("sha256", UP_SECRET_KEY).update(event.body).digest("hex") ===
+    event.headers["X-Up-Authenticity-Signature"];
 
-	if (!isAuthentic) {
-		return {
-			statusCode: 401,
-			body: "Could not verify webhook",
-		};
-	}
+  if (!isAuthentic) {
+    return {
+      statusCode: 401,
+      body: "Could not verify webhook",
+    };
+  }
 
-	const json: { data: UpObject<UpWebhookEvent> } = JSON.parse(event.body);
+  const json: { data: UpObject<UpWebhookEvent> } = JSON.parse(event.body);
 
-	const eventType = json.data.attributes.eventType;
+  const eventType = json.data.attributes.eventType;
 
-	if (!["TRANSACTION_DELETED", "TRANSACTION_CREATED"].includes(eventType)) {
-		return {
-			statusCode: 200,
-			body: "⛔",
-		};
-	}
+  if (!["TRANSACTION_DELETED", "TRANSACTION_CREATED"].includes(eventType)) {
+    return {
+      statusCode: 200,
+      body: "⛔",
+    };
+  }
 
-	let transaction;
-	try {
-		transaction = await Axios.get<{ data: UpObject<UpTransaction> }>(
-			`https://api.up.com.au/api/v1/transactions/${json.data.relationships.transaction.data.id}/`,
-			{
-				headers: {
-					"Authorization": `Bearer ${UP_BEARER_TOKEN}`,
-				},
-			}
-		);
+  let transaction;
+  try {
+    transaction = await Axios.get<{ data: UpObject<UpTransaction> }>(
+      `https://api.up.com.au/api/v1/transactions/${json.data.relationships.transaction.data.id}/`,
+      {
+        headers: {
+          "Authorization": `Bearer ${UP_BEARER_TOKEN}`,
+        },
+      }
+    );
 
-		if (eventType === "TRANSACTION_CREATED") {
-			await addTransaction(transaction.data.data);
-		} else {
-			await removeTransaction(transaction.data.data);
-		}
-	} catch (e) {
-		return {
-			statusCode: 500,
-			body: e,
-		};
-	}
+    if (eventType === "TRANSACTION_CREATED") {
+      await addTransaction(transaction.data.data);
+    } else {
+      await removeTransaction(transaction.data.data);
+    }
+  } catch (e) {
+    return {
+      statusCode: 500,
+      body: e,
+    };
+  }
 
-	return {
-		statusCode: 200,
-		body: "👌",
-	};
+  return {
+    statusCode: 200,
+    body: "👌",
+  };
 };
